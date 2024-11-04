04802e3152713519939e82e515aed7760354f58e
Soorya003 <sooryasoor003@gmail.com>
2024-11-04 13:06:01 +0530

    updated

--- frontend/src/components/Resume.jsx
+++ frontend/src/components/Resume.jsx
@@ -7,7 +7,7 @@ export default function Resume(){
         <div className='md:w-1/2 flex justify-center'>
             <div className='flex flex-col justify-center text-white'>
                 <h1 className='text-4xl border-b-4 w-[140px] mb-5 font-bold'>Resume</h1>
-                <p className='pb-5'>You can view my resume <a className='btn' href="https://drive.google.com/file/d/1kDKBJhLO0U1bJb3D0ozWbVg8gW5pqqdw/view?usp=drive_link"> View</a></p>
+                <p className='pb-5'>You can view my resume <a className='btn' href="https://drive.google.com/file/d/19gEvKhzpXZEGIXbGYmgO7LgjXAVJnqnR/view?usp=drive_link"> View</a></p>
                             </div>
         </div>
     </section>
